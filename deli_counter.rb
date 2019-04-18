d140164dd60990e65c340c2b8f8c523c584cac00
Aniket Patel <aniketupatel1@gmail.com>
2019-04-18 06:38:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  new_array = []
+  if array.length == 0 
+    puts "The line is currently empty."
+
+  else array.each.with_index(1) do |name, index|
+      new_array.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{new_array.join(" ")}"
+  end
+  
