c03f74c8d07596c5eb77d0bcb58c28415eda58e1
Aniket Patel <aniketupatel1@gmail.com>
2019-04-18 06:38:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def line(array)
   new_array = []
-  if array.length == 0 
+  if array.length == 0
     puts "The line is currently empty."
 
   else array.each.with_index(1) do |name, index|
